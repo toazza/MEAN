c82cd15f94b287ffe08db80ec376b9a857359564
jjduarte <duarte.jpdl@gmail.com>
2015-05-25 23:30:20 -0300

    bootstrap integration

--- catalogo-filmes/app.js
+++ catalogo-filmes/app.js
@@ -22,7 +22,7 @@ app.use(expressSession({
 	saveUninitialized: false
 }));
 app.use(bodyParser.urlencoded({
-	extended: true
+	extended: true 
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
